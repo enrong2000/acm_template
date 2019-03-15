ed1bdc52c9dd60e1dbe8a03f007ddf6f7f04b308
Frank <frankli0324@hotmail.com>
2019-03-15 11:43:14 +0800

    edit match function of kmp

--- code/string_processing/single_pattern_match.cpp
+++ code/string_processing/single_pattern_match.cpp
@@ -30,13 +30,13 @@ Dependencies:
     std::vector<int> build_next(std::string pattern);
 Algorithm: KMP
 Time Complexity: O(n+k)
-Remarks:
+Remarks: 在调用build_next函数获得next数组后传入到第三个参数
 Author: Frank
 */
 std::vector<int> get_matches_kmp(const std::string &src,
-                                 const std::string &pattern) {
-    auto next = build_next(pattern);
-
+                                 const std::string &pattern,
+                                 const std::vector<int> &next) {
+    //auto next = build_next(pattern);
     std::vector<int> match_pos;
     int index_src = 0, index_pat = 0;
     while (index_src < src.size()) {
